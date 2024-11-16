4d1e6651ed9f7f3ac7aaca237090d6753f59ad58
Rajib <rajib682@gmail.com>
2024-11-17 02:08:32 +0600

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="robots" content="index, follow" />
         <link rel="icon" href="/images/favicon.png" />
-        <link rel="apple-touch-icon" href="/images/Logo.png" />
+        <link rel="apple-touch-icon" href="/images/favicon.png" />
         <meta name="google-site-verification" content="A-0uOMkW3z-tI9yLRH8hK1mhZWI46c23K9UzMPnHlkU" />
       </Head>
       <Component {...pageProps} />
